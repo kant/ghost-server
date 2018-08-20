315298ae3a6a83da1172de82602ae283ca1d78dc
Charlie Cheever <ccheever@gmail.com>
2018-08-20 09:57:14 +0100

    db queries work, timing, updated repl

---
+++ db.js
@@ -0,0 +1,36 @@
+let pg = require('pg');
+
+let secret = require('./secret');
+let time = require('./time');
+
+let pool = new pg.Pool({
+  ...secret.postgres,
+  database: secret.postgres.instance,
+});
+
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('[pg] Unexpected error on idle client', err);
+  // process.exit(-1);
+});
+
+async function queryAsync(...args) {
+  let tk = time.start();
+  let client = await pool.connect();
+  time.end(tk, 'db-connect', { threshold: 10 });
+  try {
+    let tk = time.start();
+    let result = await client.query(...args);
+    time.end(tk, 'db-query', { threshold: 0, message: args[0] });
+    return result;
+  } finally {
+    client.release();
+  }
+}
+
+module.exports = {
+  queryAsync,
+  pg,
+  pool,
+};
---
+++ secret.js
@@ -0,0 +1,12 @@
+try {
+  module.exports = require('ghost-secret');
+} catch (e) {
+  try {
+    module.exports = require('../ghost-secret');
+  } catch (e2) {
+    console.error(
+      "Couldn't load ghost-secret. Make a symlnk in this directory to where the ghost-secret private repo is."
+    );
+    throw new Error(e + '\n' + e2);
+  }
+}
