f799151575baa4ba98c6ba29efd7022862b966cc
Charlie Cheever <ccheever@gmail.com>
2018-09-11 15:56:32 -0700

    Simplifying the way the server starts
    I made it too complicated before I think

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 let time = require('@expo/time');
-
-// Timers; a little weird to have them be module-level globals but its the 
-// best way to time things in a way that captures the time taken to require code too
-let _tk = time.start();
-let _tkReady = time.start();
+let _tkLoaded = time.start();
+let _tkPrimedAndStarted = time.start();
 
 let bodyParser = require('body-parser');
 let cors = require('cors');
@@ -16,12 +13,11 @@ let Api = require('./Api');
 let db = require('./db');
 
 async function serveAsync(port) {
-  let serverState = 'starting';
   let app = express();
   app.use(cors());
   app.use(bodyParser.json());
   app.get('/status', async (req, res) => {
-    res.json({serverState, });
+    res.json({ status: 'OK' });
   });
   app.post('/api', thinServer(Api));
   app.get('/', async (req, res) => {
@@ -40,25 +36,19 @@ async function serveAsync(port) {
     );
   });
 
-  // Start the server and create a Promise for when its done starting
+  // Report the time it takes to load all the code separate 
+  // from the time it takes to connect to the database
+  time.end(_tkLoaded, 'loaded');
+
+  // Make a connection to the database so its ready to go
+  await db.queryAsync('SELECT 1 AS primed');
+
+  // Start the server
   port = port || process.env.PORT || 1380;
-  let listenA = new Promise((resolve, reject) => {
-    app.listen(port, () => {
-      time.end(_tk, 'server-start');
-      console.log('Ghost server listening on port ' + port);
-      resolve();
-    });
+  app.listen(port, () => {
+    time.end(_tkPrimedAndStarted, 'server-start');
+    console.log('Ghost server listening on port ' + port);
   });
-
-  // Make a simple query to the database to force a connection, etc. 
-  let primeA = db.queryAsync('SELECT 1 AS primed');
-
-  // Wait for the server to start and the priming query to complete
-  await Promise.all([listenA, primeA]);
-
-  // Server is now ready
-  serverState = 'ready';
-  time.end(_tkReady, 'ready');
 }
 
 module.exports = serveAsync;
