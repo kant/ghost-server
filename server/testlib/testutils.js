4f4501ab498c7d41fbfc23cd32a05d6e0f95ad9e
Charlie Cheever <ccheever@gmail.com>
2018-09-29 13:26:37 -0700

    Checking for pg_dump when it is needed for tests

--- server/testlib/testutils.js
+++ server/testlib/testutils.js
@@ -7,9 +7,32 @@ let time = require('@expo/time');
 
 let secret = require('../secret');
 
+async function pg_dumpIsInstalledAsync() {
+  try {
+    await spawnAsync('pg_dump', ['--version']);
+  } catch (e) {
+    if (e.message.endsWith('ENOENT')) {
+      return false;
+    }
+    throw e;
+  }
+  return true;
+}
+
 async function getProductionDatabaseSchemaAsync() {
   console.log('db-schema-dump: starting...');
   let tk = time.start();
+
+  if (!await pg_dumpIsInstalledAsync()) {
+    throw Error(`
+      pg_dump is required for running tests but is not installed.
+
+      To fix this, you can run:
+
+        brew install postgres
+
+    `);
+  }
   let pg_dump = path.resolve(__dirname, 'pg_dump');
   let result = await spawnAsync(pg_dump, ['--schema-only']);
   let [stdout, stderr] = result.output;
@@ -53,4 +76,5 @@ async function createDatabaseAsync(dbName) {
 module.exports = {
   getProductionDatabaseSchemaAsync,
   createDatabaseAsync,
+  pg_dumpIsInstalledAsync,
 };
